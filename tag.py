c9dad925f8cf8749ecf9c558445d42fb7a880b73
andrewtgomez96 <andrewtgomez96@gmail.com>
2019-03-07 18:30:17 -0800

    Finished the post route for tags

--- tag.py
+++ tag.py
@@ -1,9 +1,9 @@
 #tag
 # 1 Add an article with a new tag
-# 2 Add another tag to the article
-# 3 Add a tag to an article that doesn’t exist
+# 2 Add another tag to the article and # 3 Add a tag to an article that doesn’t exist
 # 4 Delete one or more of the tags from the article
 # 5 List all articles with the new tag
+# 6 Retrieve the tags for an individual URL
 
 
 from flask import Flask, current_app, request, jsonify
@@ -14,6 +14,10 @@ from flask.cli import with_appcontext
 app = Flask(__name__)
 db = SQLite3(app)
 
+'''
+if you run 'flask init-db-command' it will drop the current database and reinitialize it from
+the scheme.sql file which resets the db, inserting 3 rows in each table
+'''
 @app.cli.command()
 @with_appcontext
 def init_db_command():
@@ -22,33 +26,48 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 def init_db():
-    
     with current_app.open_resource('schema.sql') as f:
         db.connection.executescript(f.read().decode('utf8'))
 
-#1 and 2 and 3
-@app.route("/article/<string:articleId>/tag/<string:tag>", methods=['POST', 'PATCH', 'DELETE'])
-def addTags(articleId, tag):
+#1
+@app.route("/article/tag/<string:tag>", methods=['POST'])
+def addArtTag(tag):
     cur = db.connection.cursor()
-    if(request.method == 'POST'):
-        return jsonify(cur.execute("select * from User").fetchall())
-    if(request.method == 'PATCH'):
-        cur.execute("""INSERT INTO User (username, password)
-                    VALUES ('Andrew', 'disme')""")
-        db.connection.commit()
-        if(articleId != "me"):
-            return "http status code blah blah"
-        return jsonify(cur.execute("select * from User").fetchall())
+    userName = request.form.get('userName')
+    title = request.form.get('title')
+    body = request.form.get('body')
+    insertArticle = (userName, title, body)
 
+    cur.execute("INSERT INTO Article (userName, title, body, commentCount) VALUES (?, ?, ?, 0)", insertArticle)
+    cur.execute("SELECT artId FROM Article WHERE title = ? ", (title,))
+    artId = cur.fetchone()[0]
+    insertTag = (tag, artId)
+    cur.execute("INSERT INTO Tag (tag, artId) VALUES (?, ?)", insertTag)
+    db.connection.commit()
+    return jsonify(artId), 201
+
+#2 and 3
+@app.route("/article/<string:articleId>/tag/<string:tag>", methods=['PUT'])
+def addTag(articleId, tag):
+    cur = db.connection.cursor()
+    cur.execute("""INSERT INTO User (username, password)
+                VALUES ('Andrew', 'disme')""")
+    db.connection.commit()
+    if(articleId != "me"):
+        return "http status code blah blah"
+    return jsonify(cur.execute("select * from User").fetchall())
+
+#6
 @app.route("/article/<string:articleId>/tags", methods=['GET'])
 def getTags(articleId):
     return f"grabbing all tags from {articleId} article"
-        
+
+#4  
 @app.route("/article/<string:articleId>/tag/<int:n>", methods=['DELETE'])
 def deleteTags(articleId, n):
     return f"we are deleting {n} tags from an article"
 
-#get all articles with tag
+#5
 @app.route("/articles/tag/<string:tag>", methods=['GET'])
 def getArticles(tag):
     return "List all articles with the new tag"
